cba05a9dd11dcb51c0612a5a76b2f924efec4c04
Christopher Hsiao <hsiao.christopher@gmail.com>
2015-10-09 11:00:59 -0700

    First Commit

---
+++ Assignment_2/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+	private static int size;
+	public static void main(String args[]) throws FileNotFoundException{
+		Scanner reader = new Scanner(new FileInputStream(args[0]));
+		size = reader.nextInt();
+		
+	}
+}
